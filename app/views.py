0d9881c0ea5536875278b183159cb7473b88face
yasmeen123456789 <yasmeenyassu3@gmail.com>
2023-06-01 16:46:46 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def ayisha(request):
+    return render(request,"ayisha.html")
+
+def yasmeen(request):
+    return render(request,"yasmeen.html")
